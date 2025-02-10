c51c038aab763554d9fd7f6ecb7d3727172e35e4
chlovers <chloie_day@hotmail.com>
2025-02-10 15:14:46 -0400

    button script added but not finished

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+ 
+    public void PlayButton()
+    {
+
+
+    }
+
+    public void QuitButton()
+    {
+
+
+    }
+
+    public void ResumeButton()
+    {
+
+
+    }
+
+    public void BackToMenuButton()
+    {
+
+
+    }
+
+
+}
